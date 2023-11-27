641b3b3b43804941680b13d274672d86a7f6ceb8
Harunmtn <mtn.musa07@gmail.com>
2023-11-27 18:01:50 +0300

    v1

---
+++ src/WhileLoops/DoWhileLoops.java
@@ -0,0 +1,20 @@
+package WhileLoops;
+
+public class DoWhileLoops {
+    public static void main(String[] args) {
+
+        /*
+        while dongusu kullanarak 1'den 10'a kadar olan sayilari ekrana yazdiracak bir java
+        programi yaziniz.
+         */
+
+        int sayi = 1;
+
+        while (sayi<=10){
+            System.out.println(sayi);
+            sayi++;
+        }
+
+
+    }
+}
--- src/WhileLoops/WhileLoops2.java
+++ src/WhileLoops/WhileLoops2.java
@@ -1,4 +1,9 @@
 package WhileLoops;
 
 public class WhileLoops2 {
+
+    public static void main(String[] args) {
+
+
+    }
 }
