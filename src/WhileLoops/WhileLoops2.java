9ae4ae59796b414dd97b2c28250ff18ab5ae9bc8
Harunmtn <mtn.musa07@gmail.com>
2023-11-27 18:34:37 +0300

    v1

--- src/WhileLoops/WhileLoops2.java
+++ src/WhileLoops/WhileLoops2.java
@@ -1,8 +1,22 @@
 package WhileLoops;
 
+import java.util.Scanner;
+
 public class WhileLoops2 {
 
     public static void main(String[] args) {
+        /*
+        1'den 20'ye kadarolan cift sayilari ekrana yazdiran bir java programi yaziniz
+         */
+
+        int sayi =1;
+        while (sayi<=20){
+            if (sayi%2==0){
+                System.out.println(sayi);
+            }
+            sayi++;
+        }
+
 
 
     }
