bb7f0b2ea7973f8ae3aee29dd2a5f4fc9778b37d
Harunmtn <mtn.musa07@gmail.com>
2023-12-11 00:47:23 +0300

    v1

---
+++ src/WhileLoops/AlisverisListesi.java
@@ -0,0 +1,21 @@
+package WhileLoops;
+
+import com.sun.tools.javac.Main;
+
+public class AlisverisListesi {
+
+
+    public static void main(String[] args) {
+
+        /*
+    Alışveriş Listesi Uygulaması:
+      -Alışveriş yapılacak ürünleri içeren bir liste oluşturun.
+      -Kullanıcıya ürün ekleme, çıkarma ve listeyi görüntüleme seçenekleri sunan bir menü oluşturun.
+      -Listeyi düzenleme ve sıralama yetenekleri ekleyin.
+     */
+
+    }
+
+
+
+}
--- src/WhileLoops/DoWhileLoop1.java
+++ src/WhileLoops/DoWhileLoop1.java
@@ -1,2 +1,42 @@
-package WhileLoops;public class DoWhileLoop1 {
+package WhileLoops;
+
+import java.util.Scanner;
+
+public class DoWhileLoop1 {
+
+    public static void main(String[] args) {
+        /*
+        kullanicidan baslangic ve bitis sayisi alip ,bu araliktaki sayilari toplayip
+        ve sonucu ekrana yazdirin kullanici baslangic sayisini bitis sayisindan buyuk girene kadar bu islemi
+        tekrar etsin.(do-while)
+         */
+        Scanner scanner = new Scanner(System.in);
+
+        int baslangic, bitis;
+
+        do {
+            System.out.print("Başlangıç sayısını giriniz: ");
+            baslangic = scanner.nextInt();
+
+            System.out.print("Bitiş sayısını giriniz: ");
+            bitis = scanner.nextInt();
+
+            if (bitis <= baslangic) {
+                System.out.println("Hatalı giriş! Bitiş sayısı başlangıç sayısından büyük olmalı.");
+            }
+
+        } while (bitis <= baslangic);
+
+
+        int toplam = 0;
+        int current = baslangic;
+
+        do {
+            toplam += current;
+            current++;
+        } while (current <= bitis);
+
+        System.out.println("Toplam: " + toplam);
+    }
+
 }
