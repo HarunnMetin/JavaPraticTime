7320ed1635e68dc1e90347f8c7674953d3b43b67
Harunmtn <mtn.musa07@gmail.com>
2023-12-19 02:49:21 +0300

    java

--- src/ArrayList/TekSayilariCikar.java
+++ src/ArrayList/TekSayilariCikar.java
@@ -1,5 +1,8 @@
 package ArrayList;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class TekSayilariCikar {
 
     public static void main(String[] args) {
@@ -8,5 +11,22 @@ public class TekSayilariCikar {
         Kullanıcıdan alınan bir tamsayı listesindeki tek sayıları çıkaran
         ve geri kalan elemanları yeni bir listede saklayan bir Java programı
          */
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Sayıları aralarında boşluk bırakarak girin: ");
+        String input = scanner.nextLine();
+
+
+        String[] sayilarString = input.split(" ");
+
+        ArrayList<Integer> ciftSayilar = new ArrayList<>();
+
+        for (String sayiString : sayilarString) {
+            int sayi = Integer.parseInt(sayiString);
+            if (sayi % 2 == 0) {
+                ciftSayilar.add(sayi);
+            }
+        }
+        System.out.println("Girilen sayılardan çift olanlar: " + ciftSayilar);
     }
 }
